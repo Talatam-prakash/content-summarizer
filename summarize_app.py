68b4368af239d2c629dec45fee040a4f0fb69fe3
Talatam Prakash Naidu <134668494+Talatam-prakash@users.noreply.github.com>
2025-07-20 14:54:01 +0530

    Update summarize_app.py

--- summarize_app.py
+++ summarize_app.py
@@ -1,3 +1,6 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import validators, streamlit as st
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.document_loaders import YoutubeLoader,UnstructuredURLLoader
